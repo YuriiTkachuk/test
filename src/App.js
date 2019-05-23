1a97f50b0478f906686d8ae6dc66c59d0a60031c
yurii <tka4yk1848@gmail.com>
2019-05-23 18:04:15 +0300

    map v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,127 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+import json from './clients.json';
+import Geocode from 'react-geocode';
+import {Gmaps, Marker} from 'react-gmaps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const api = "AIzaSyAR3ZusMJ8YPkAkbnsh8cXVBuzWJ7Sk3C0";
+Geocode.setApiKey(api);
+const params = {v: '3.exp', key: api};
+const customers = json.Customers;
+const counter = (arr) => {
+  return arr.reduce(function (p, c) { p[c] = (p[c] || 0) + 1; return p }, {})
+};
 
-export default App;
+const countries = counter(customers.map(w => w.Country));
+const sortCountries = Object.entries(countries).sort((a,b) => b[1] - a[1]).map(w => w[0]);
+let cities = counter(customers.filter(w => w.Country === sortCountries[0]).map(w => w.City));
+let sortCities = Object.entries(cities).sort((a,b) => b[1] - a[1]).map(w => w[0]);
+let sortCompany = customers.filter(w => w.Country === sortCountries[0] && w.City === sortCities[0]).map(w => w.CompanyName).sort();
+
+console.log(sortCompany)
+
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: 0,
+            lng: 0,
+            country: sortCountries[0],
+            city: sortCities[0],
+            company: sortCompany[0],
+        };
+    }
+
+    componentDidMount() {
+        const { country, city, company } = this.state;
+        const address = customers.filter(w => w.CompanyName === company && w.City === city && w.Country === country)[0]['Address']
+        Geocode.fromAddress(`${city} ${address}`).then(
+            response => {
+                this.setState({
+                    lat: response.results[0].geometry.location.lat,
+                    lng: response.results[0].geometry.location.lng,
+                })
+            },
+            error => {
+                console.error(error);
+            }
+        );
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        const { country, city, company } = this.state;
+        console.log(prevState)
+        if (country !== prevState.country) {
+            this.setState({
+                city: sortCities[0],
+            })
+        }
+        if (company !== prevState.company) {
+            const address = customers.filter(w => w.CompanyName === company && w.City === city && w.Country === country)[0]['Address']
+            console.log(address)
+            Geocode.fromAddress(`${city}, ${address}`).then(
+                response => {
+                    this.setState({
+                        lat: response.results[0].geometry.location.lat,
+                        lng: response.results[0].geometry.location.lng,
+                    })
+                },
+                error => {
+                    console.error(error);
+                }
+            );
+        }
+    };
+
+    onMapCreated(map) {
+        map.setOptions({
+            disableDefaultUI: true
+        });
+    }
+
+    handleClick = (e, name) => {
+        e.persist();
+
+        this.setState({
+            [name]: e.target.textContent,
+        });
+    };
+
+    render() {
+        const { lat, lng, country, city, company } = this.state;
+        cities = counter(customers.filter(w => w.Country === country).map(w => w.City));
+        sortCities = Object.entries(cities).sort((a,b) => b[1] - a[1]).map(w => w[0]);
+        sortCompany = customers.filter(w => w.Country === country && w.City === city).map(w => w.CompanyName).sort();
+        return (
+            <div className="container">
+                <div className="content">
+                    <div className="col">
+                        {sortCountries.map((c, i) => <span key={i} className={c === country ? 'active' : ''} onClick={(e) => this.handleClick(e, 'country')}>{c}</span>)}
+                    </div>
+                    <div className="col">
+                        {sortCities.map((c, i) => <span key={i} className={c === city ? 'active' : ''} onClick={(e) => this.handleClick(e, 'city')}>{c}</span>)}
+                    </div>
+                    <div className="col">
+                        {sortCompany.map((c, i) => <span key={i} className={c === company ? 'active' : ''} onClick={(e) => this.handleClick(e, 'company')}>{c}</span>)}
+                    </div>
+                    <Gmaps
+                      width={'300px'}
+                      height={'400px'}
+                      lat={lat}
+                      lng={lng}
+                      zoom={12}
+                      params={params}
+                      onMapCreated={this.onMapCreated}>
+                          <Marker
+                            lat={lat}
+                            lng={lng}
+                            draggable={true} />
+                    </Gmaps>
+                  </div>
+            </div>
+        );
+    }
+
+};
